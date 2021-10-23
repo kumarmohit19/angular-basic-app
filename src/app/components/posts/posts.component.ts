a9ec4647421e5b04566d1f19c401cf0a7ed1c1c4
Kumar <mohitnevatru@gmail.com>
2021-10-23 08:40:37 +0530

    post model, component, service and linking post with post-form using emit

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/Post';
+import { PostsService } from 'src/app/services/posts.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css'],
+})
+export class PostsComponent implements OnInit {
+  posts: Post[];
+  currentPost: Post = {
+    id: 0,
+    title: '',
+    body: '',
+  };
+  isEdit: boolean = false;
+
+  constructor(private postService: PostsService) {}
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
+
+  onNewPost(post: Post) {
+    this.posts.unshift(post);
+  }
+
+  editPost(post: Post) {
+    this.currentPost = post;
+    this.isEdit = true;
+  }
+
+  onUpdatedPost(post: Post) {
+    this.posts.forEach((curr, index) => {
+      if (post.id == curr.id) {
+        this.posts.splice(index, 1);
+        this.posts.unshift(post);
+        this.isEdit = false;
+        this.currentPost = {
+          id: 0,
+          title: '',
+          body: '',
+        };
+      }
+    });
+  }
+
+  removePost(post: Post) {
+    if (confirm('Are You Sure')) {
+      this.postService.removePost(post.id).subscribe(() => {
+        this.posts.forEach((curr, index) => {
+          if (post.id == curr.id) {
+            this.posts.splice(index, 1);
+          }
+        });
+      });
+    }
+  }
+}
