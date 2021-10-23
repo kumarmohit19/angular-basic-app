9375d3506b7714356b15f77bb24bde6461581ca2
Kumar <mohitnevatru@gmail.com>
2021-10-23 08:38:59 +0530

    fetching user data using service class and obervables

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  users: User[];
+  data: Observable<any>;
+
+  constructor() {
+    this.users = [
+      {
+        firstName: 'Jhon',
+        lastName: 'Doe',
+        email: 'jhon@gmail.com',
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true,
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Jhonson',
+        email: 'kevin@yahoo.com',
+        isActive: false,
+        registered: new Date('03/11/2018 06:20:00'),
+        hide: true,
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        email: 'karen@gmail.com',
+        isActive: true,
+        registered: new Date('11/02/2016 10:30:00'),
+        hide: true,
+      },
+    ];
+  }
+
+  getUsers(): Observable<User[]> {
+    return of(this.users);
+  }
+
+  addUser(user: User) {
+    this.users.unshift(user);
+  }
+
+  getData() {
+    this.data = new Observable((observer) => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1000);
+      setTimeout(() => {
+        observer.next(2);
+      }, 2000);
+      setTimeout(() => {
+        observer.next(3);
+      }, 3000);
+      setTimeout(() => {
+        observer.next(4);
+      }, 4000);
+    });
+    return this.data;
+  }
+}
